9a69bfd5b6beaf13125a21283b7d6e2a9c2c78fe
Pascal Mathis <mail@pascalmathis.com>
2019-07-28 14:18:07 +0200

    *: Cleanup unused custom Python extensions (replaced by stdlib)

--- _states/ss.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, print_function, generators, unicode_literals
-
-
-def resource(name):
-    return __states__['test.configurable_test_state'](
-        name=name, changes=True, result=True, comment='Resource Declaration')
